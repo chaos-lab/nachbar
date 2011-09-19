8e2bde5d688b0cbd6bfac3c4346187646b640562
liufengyun <liufengyunchina@gmail.com>
2011-09-19 23:41:15 +0800

    joyent start file

---
+++ server.js
@@ -0,0 +1,20 @@
+require('coffee-script')
+
+global.nachbar = {}
+
+// config
+nachbar.config = require('./utils/config')
+
+// models
+nachbar.User = require('./models/user')
+
+// logic
+nachbar.geocenter = require('./logic/geocenter')
+
+// app
+webapp = require('./logic/webserver')
+sockapp = require('./logic/socketserver')
+
+webapp.start()
+sockapp.start(webapp)
+
